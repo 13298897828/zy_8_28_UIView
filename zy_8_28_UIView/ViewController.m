65a3fd367cde1b26c826c57d0379ca7a1f2c5ebc
Tianqi Zhang <1120404010@qq.com>
2015-08-28 17:41:33 +0800

    Initial Commit

---
+++ zy_8_28_UIView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zy_8_28_UIView
+//
+//  Created by 张天琦 on 15/8/28.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
